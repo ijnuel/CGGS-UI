cf812ef86b38c65a8de59458744c8171f6b99c8d
ijnuel <45212920+ijnuel@users.noreply.github.com>
2025-12-16 16:23:35 +0100

    Add Angular Material Moment adapter to webapp module
    Added @angular/material-moment-adapter dependency and configured MatMomentDateModule in webapp.module.ts. Set date locale to 'en-GB' and enabled UTC for the date adapter.

--- src/app/pages/webapp/webapp.module.ts
+++ src/app/pages/webapp/webapp.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 import { WebappRoutingModule } from './webapp-routing.module';
 import { WebappComponent } from './webapp.component';
 import { SharedModule } from "../../shared/shared.module";
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import {
+  MatMomentDateModule,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS
+} from '@angular/material-moment-adapter';
 
 @NgModule({
   declarations: [WebappComponent],
-  imports: [CommonModule, WebappRoutingModule, SharedModule],
+  imports: [CommonModule, WebappRoutingModule, SharedModule, MatMomentDateModule],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } }
+  ],
 })
 export class WebappModule {}
