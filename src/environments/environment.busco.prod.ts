bf33d276935d8b1bbb1813aa79f9c5285ce34b10
sayTG <busarithienkie@gmail.com>
2026-03-24 00:40:19 +0100

    feat: env changes

--- src/environments/environment.busco.prod.ts
+++ src/environments/environment.busco.prod.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: 'https://api.chosenschool.org/api',
   // baseUrl: 'https://ijesudunsin-002-site1.ftempurl.com/api',
   schoolName: 'Busco School',
